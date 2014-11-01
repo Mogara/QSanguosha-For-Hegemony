f2e0c65c3ef235d8c6e3dbeba9cf07ca8743e337
YanGuam <rara@qsanguosha.org>
2014-11-02 03:23:16 +0800

    Resolve #220, enable preshowing automatically in on-line game

--- src/core/settings.h
+++ src/core/settings.h
@@ -104,7 +104,7 @@ public:
     bool EnableDoubleClick;
     bool EnableAutoSaveRecord;
     bool NetworkOnly;
-    bool EnableAutoPreshowInConsoleMode;
+    bool EnableAutoPreshow;
     int OperationTimeout;
     bool OperationNoLimit;
     bool EnableEffects;
