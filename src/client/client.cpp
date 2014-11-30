e2cc4248e5b22cf5da8d896b2240f98115d56659
YanGuam <rara@qsanguosha.org>
2014-11-30 23:47:22 +0800

    [0.8.0beta3][bug][-1][调整不询问无懈可击的操作逻辑][network|ui]
    [ver][type(bug|enhancement|story)][issue index(-1即无编号)][log][影响范围]

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -768,12 +768,10 @@ void Client::setNullification(const QVariant &str) {
     QString astr = str.toString();
     if (astr != ".") {
         if (m_noNullificationTrickName == ".") {
-            m_noNullificationThisTime = false;
             m_noNullificationTrickName = astr;
             emit nullification_asked(true);
         }
-    }
-    else {
+    } else {
         m_noNullificationThisTime = false;
         m_noNullificationTrickName = ".";
         emit nullification_asked(false);
@@ -1038,10 +1036,8 @@ void Client::askForNullification(const QVariant &arg) {
         }
     }
     if (m_noNullificationThisTime && m_noNullificationTrickName == trick_name) {
-        if (trick_card->isKindOf("AOE") || trick_card->isKindOf("GlobalEffect")) {
-            onPlayerResponseCard(NULL);
-            return;
-        }
+        onPlayerResponseCard(NULL);
+        return;
     }
 
     if (source == NULL) {
