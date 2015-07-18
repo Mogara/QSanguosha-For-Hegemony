6a0ceabcdc1dec7d2cad7c3fea8221e45fe77a42
Yanguam Siliagim <Rara@mogara.org>
2015-07-18 14:55:47 +0800

    tab @Xusine1131 五毛拿来

--- src/server/roomthread.cpp
+++ src/server/roomthread.cpp
@@ -223,11 +223,11 @@ bool RoomThread::trigger(TriggerEvent triggerEvent, Room *room, ServerPlayer *ta
     try {
         QList<const TriggerSkill *> triggered;
         QList<const TriggerSkill *> &skills = skill_table[triggerEvent];
-//         foreach (const TriggerSkill *skill,skills){
-//             skill->setCurrentPriority(skill->getDynamicPriority(triggerEvent));
-//         }
+        //         foreach (const TriggerSkill *skill,skills){
+        //             skill->setCurrentPriority(skill->getDynamicPriority(triggerEvent));
+        //         }
 
-		qStableSort(skills.begin(), skills.end(), [triggerEvent](const TriggerSkill *a, const TriggerSkill *b){return a->getDynamicPriority(triggerEvent) > b->getDynamicPriority(triggerEvent); });
+        qStableSort(skills.begin(), skills.end(), [triggerEvent](const TriggerSkill *a, const TriggerSkill *b) {return a->getDynamicPriority(triggerEvent) > b->getDynamicPriority(triggerEvent); });
 
         do {
             trigger_who.clear();
@@ -595,7 +595,7 @@ void RoomThread::addTriggerSkill(const TriggerSkill *skill)
     foreach (const TriggerEvent &triggerEvent, events) {
         QList<const TriggerSkill *> &table = skill_table[triggerEvent];
         table << skill;
-		qStableSort(table.begin(), table.end(), [triggerEvent](const TriggerSkill *a, const TriggerSkill *b){return a->getDynamicPriority(triggerEvent) > b->getDynamicPriority(triggerEvent); });
+        qStableSort(table.begin(), table.end(), [triggerEvent](const TriggerSkill *a, const TriggerSkill *b) {return a->getDynamicPriority(triggerEvent) > b->getDynamicPriority(triggerEvent); });
     }
 
     if (skill->isVisible()) {
