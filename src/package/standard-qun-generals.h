41a78d525573a54f782640e97eba28a6861d40f4
Amira <lten@outlook.com>
2015-07-24 00:52:53 +0800

    哦艹我的天残手

--- src/package/standard-qun-generals.h
+++ src/package/standard-qun-generals.h
@@ -48,6 +48,15 @@ public:
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
+class LirangCard : public SkillCard
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE LirangCard();
+    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
+};
+
 class LuanwuCard : public SkillCard
 {
     Q_OBJECT
