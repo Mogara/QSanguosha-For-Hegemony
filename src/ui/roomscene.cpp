1f3e37d4872ec08143943302d3af754b4423bb61
weirdouncle <masterlong@tom.com>
2016-08-14 20:47:12 +0800

    修手滑

--- src/ui/roomscene.cpp
+++ src/ui/roomscene.cpp
@@ -139,7 +139,6 @@ RoomScene::RoomScene(QMainWindow *main_window)
     connect(Self, &ClientPlayer::general2_changed, dashboard, &Dashboard::updateSmallAvatar);
     connect(dashboard, &Dashboard::card_selected, this, &RoomScene::enableTargets);
     connect(dashboard, &Dashboard::card_to_use, this, &RoomScene::doOkButton);
-    connect(dashboard, &Dashboard::card_unselect, this, &RoomScene::cleanLiegong);
 
     connect(Self, &ClientPlayer::pile_changed, dashboard, &Dashboard::updatePile);
 
@@ -1874,7 +1873,7 @@ void RoomScene::getCards(int moveId, QList<CardsMoveStruct> card_moves)
         for (int j = 0; j < cards.size(); j++) {
             CardItem *card = cards[j];
             card->setFlag(QGraphicsItem::ItemIsMovable, false);
-            if (!reason.m_skillName.isEmpty() && movement.from && movement.to_place != Player::PlaceHand && !movement.to_place != Player::PlaceSpecial) {
+            if (!reason.m_skillName.isEmpty() && movement.from && movement.to_place != Player::PlaceHand && movement.to_place != Player::PlaceSpecial) {
                 if (movement.from->getGeneral()->hasSkill(reason.m_skillName))
                     card->showAvatar(movement.from->getGeneral(), reason.m_skillName);
                 else if (movement.from->getGeneral2()->hasSkill(reason.m_skillName))
